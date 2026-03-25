dbe458baddbdc9fd04808bbd4d14fe08cf4fee8c
Mohammed Hamza Malik <m.h.m.i.malik@gmail.com>
2026-03-24 23:15:28 -0500

    the main application file

---
+++ src/main/java/com/hamza/apikeymanager/ApikeymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.hamza.apikeymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApikeymanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApikeymanagerApplication.class, args);
+    }
+
+}
