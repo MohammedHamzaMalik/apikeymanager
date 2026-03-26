297268a4c51424c7f0b5597425984716334807b9
Mohammed Hamza Malik <m.h.m.i.malik@gmail.com>
2026-03-26 00:05:18 -0500

    fixed the code as the API version got updated

--- src/main/java/com/hamza/apikeymanager/security/JwtUtil.java
+++ src/main/java/com/hamza/apikeymanager/security/JwtUtil.java
@@ -1,44 +1,43 @@
 package com.hamza.apikeymanager.security;
 
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
     private static final String SECRET = "thisIsASecretKeyThatIsLongEnoughForHS256Algorithm!";
-    private static final long EXPIRY = 1000*60*60*24; // 24 hrs
+    private static final long EXPIRY = 1000 * 60 * 60 * 24;
 
-    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+    private final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes());
 
     public String generateToken(String email) {
         return Jwts.builder()
-                .setSubject(email)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis()*EXPIRY))
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + EXPIRY))
                 .signWith(key)
                 .compact();
     }
 
-    public String extractEmail(String token){
-        return Jwts.parserBuilder()
-                .setSigningKey(key)
+    public String extractEmail(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
                 .build()
-                .parseClaimsJws(token)
-                .getBody()
+                .parseSignedClaims(token)
+                .getPayload()
                 .getSubject();
     }
 
-    public boolean isTokenValid(String token){
+    public boolean isTokenValid(String token) {
         try {
             extractEmail(token);
             return true;
-        } catch (JwtException e){
+        } catch (JwtException e) {
             return false;
         }
     }
