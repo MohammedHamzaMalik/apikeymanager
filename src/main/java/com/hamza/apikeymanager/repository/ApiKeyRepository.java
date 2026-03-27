759e417a6f7c8b4da6da026a54b90a500fd81e72
Mohammed Hamza Malik <m.h.m.i.malik@gmail.com>
2026-03-26 21:35:14 -0500

    added method findByOwner to get owner from db

--- src/main/java/com/hamza/apikeymanager/repository/ApiKeyRepository.java
+++ src/main/java/com/hamza/apikeymanager/repository/ApiKeyRepository.java
@@ -3,5 +3,8 @@ package com.hamza.apikeymanager.repository;
 import com.hamza.apikeymanager.model.ApiKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ApiKeyRepository extends JpaRepository<ApiKey, Long> {
+    List<ApiKey> findByOwner(String owner);
 }
